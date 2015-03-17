c227890fdc96e3ad6e3f615779c39c343809084c
James Dallman <jrdallman@wisc.edu>
2015-03-17 18:47:08 -0500

    Fixed Images

--- core/src/club/enlight/software/states/MainMenu.java
+++ core/src/club/enlight/software/states/MainMenu.java
@@ -30,38 +30,38 @@ public class MainMenu extends State {
     {   super(sm);
         stage = new Stage();
         ImageButton.ImageButtonStyle optionsButtonStyle = new ImageButton.ImageButtonStyle();
-        optionsButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture(Gdx.files.internal("Options.jpg"))));
-        optionsButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/Options click.jpg")));
-        optionsButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/Options hover.jpg")));
+        optionsButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture(Gdx.files.internal("Button/Options.png"))));
+        optionsButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/OptionsClick.png")));
+        optionsButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/OptionsHover.png")));
         final ImageButton optionsButton = new ImageButton(optionsButtonStyle);
         optionsButton.setSize(4/14*Gdx.graphics.getWidth(), 7 / 50 * Gdx.graphics.getHeight());
         optionsButton.setPosition(7/14*Gdx.graphics.getWidth(),0);
 
         ImageButton.ImageButtonStyle playButtonStyle = new ImageButton.ImageButtonStyle();
-        playButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Play.jpg")));
-        playButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/Play click.jpg")));
-        playButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/Play hover.jpg")));
+        playButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Play.png")));
+        playButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/PlayClick.png")));
+        playButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/PlayHover.png")));
         final ImageButton playButton = new ImageButton(playButtonStyle);
         playButton.setSize(3 / 14 * Gdx.graphics.getWidth(), 7 / 50 * Gdx.graphics.getHeight());
         playButton.setPosition(0, 0);
 
         ImageButton.ImageButtonStyle exitButtonStyle = new ImageButton.ImageButtonStyle();
-        exitButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Exit.jpg")));
-        exitButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/Exit click.jpg")));
-        exitButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/Exit hover.jpg")));
+        exitButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Exit.png")));
+        exitButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/ExitClick.png")));
+        exitButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/ExitHover.png")));
         final ImageButton exitButton = new ImageButton(exitButtonStyle);
         exitButton.setSize(4 / 14 * Gdx.graphics.getWidth(), 7 / 50 * Gdx.graphics.getHeight());
         exitButton.setPosition(3/14*Gdx.graphics.getWidth(), 0);
 
         ImageButton.ImageButtonStyle loadButtonStyle = new ImageButton.ImageButtonStyle();
-        loadButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Load.jpg")));
-        loadButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/Load click.jpg")));
-        loadButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/Load hover.jpg")));
+        loadButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Load.png")));
+        loadButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/LoadClick.png")));
+        loadButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/LoadHover.png")));
         final ImageButton loadButton = new ImageButton(loadButtonStyle);
         loadButton.setSize(3 / 14 * Gdx.graphics.getWidth(), 7 / 50 * Gdx.graphics.getHeight());
         loadButton.setPosition(11/14*Gdx.graphics.getWidth(), 0);
 
-        Image background = new Image(new Texture("Menu Screen/TC Load 1.jpg"));
+        Image background = new Image(new Texture("Menu Screen/BG1.png"));
         //Implement buttons and stuff that show up when the game starts.
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         renderer = new ShapeRenderer();
