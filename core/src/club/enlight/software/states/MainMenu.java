4e9dabb7c20f0f03061c494832cad980d5171254
Kenneth S <kksiu@wisc.edu>
2015-03-24 18:51:13 -0500

    Play and Exit buttons functionality

--- core/src/club/enlight/software/states/MainMenu.java
+++ core/src/club/enlight/software/states/MainMenu.java
@@ -8,10 +8,14 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
@@ -60,6 +64,13 @@ public class MainMenu extends State {
         playButton.setSize( 3.f * buttonWidthDivider * width, heightOfButton);
         playButton.setPosition(0, 0);
 
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                sm.setState(StateManager.GAME_LEVEL);
+            }
+        });
+
         // Set up Exit Button
         ImageButton.ImageButtonStyle exitButtonStyle = new ImageButton.ImageButtonStyle();
         exitButtonStyle.up = new TextureRegionDrawable( atlas.findRegion("Exit"));
@@ -69,6 +80,13 @@ public class MainMenu extends State {
         final ImageButton exitButton = new ImageButton(exitButtonStyle);
         exitButton.setSize(3.f * buttonWidthDivider * width, heightOfButton);
         exitButton.setPosition(3.5f * buttonPositionDivider * width, 0);
+
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
 
         // Set up Load Button
         ImageButton.ImageButtonStyle loadButtonStyle = new ImageButton.ImageButtonStyle();
@@ -96,6 +114,7 @@ public class MainMenu extends State {
 
         Gdx.input.setInputProcessor(stage);
     }
+
     @Override
     public void handleInput() {
 
