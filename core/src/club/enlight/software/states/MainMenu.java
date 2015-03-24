d30b23fe6c0543444af6bbf0577cef9f58b1b84f
James Dallman <jrdallman@wisc.edu>
2015-03-24 18:07:15 -0500

    turned things and stuff into an atlas/pack.

--- core/src/club/enlight/software/states/MainMenu.java
+++ core/src/club/enlight/software/states/MainMenu.java
@@ -4,13 +4,18 @@ import club.enlight.software.handlers.StateManager;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import static com.badlogic.gdx.graphics.Texture.*;
 
 /**
  * Created by jdallman2570 on 2/23/2015.
@@ -23,34 +28,36 @@ public class MainMenu extends State {
     public MainMenu(final StateManager sm)
     {   super(sm);
         stage = new Stage();
+        TextureAtlas atlas;
+        atlas = new TextureAtlas(Gdx.files.internal("Button/ButtonPack.atlas"));
         ImageButton.ImageButtonStyle optionsButtonStyle = new ImageButton.ImageButtonStyle();
-        optionsButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture(Gdx.files.internal("Button/Options.png"))));
-        optionsButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/OptionsClick.png")));
-        optionsButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/OptionsHover.png")));
+        optionsButtonStyle.up =  new TextureRegionDrawable(atlas.findRegion("Options"));
+        optionsButtonStyle.down = new TextureRegionDrawable(atlas.findRegion("OptionsClick"));
+        optionsButtonStyle.imageOver = new TextureRegionDrawable(atlas.findRegion("OptionsHover"));
         final ImageButton optionsButton = new ImageButton(optionsButtonStyle);
         optionsButton.setSize(4/14*Gdx.graphics.getWidth(), 7 / 50 * Gdx.graphics.getHeight());
-        optionsButton.setPosition(7/14*Gdx.graphics.getWidth(),0);
+        optionsButton.setPosition(7 / 14 * Gdx.graphics.getWidth(), 0);
 
         ImageButton.ImageButtonStyle playButtonStyle = new ImageButton.ImageButtonStyle();
-        playButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Play.png")));
-        playButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/PlayClick.png")));
-        playButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/PlayHover.png")));
+        playButtonStyle.up = new TextureRegionDrawable(atlas.findRegion("Play"));
+        playButtonStyle.down = new TextureRegionDrawable(atlas.findRegion("PlayClick"));
+        playButtonStyle.imageOver = new TextureRegionDrawable(atlas.findRegion("PlayHover"));
         final ImageButton playButton = new ImageButton(playButtonStyle);
         playButton.setSize(3 / 14 * Gdx.graphics.getWidth(), 7 / 50 * Gdx.graphics.getHeight());
         playButton.setPosition(0, 0);
 
         ImageButton.ImageButtonStyle exitButtonStyle = new ImageButton.ImageButtonStyle();
-        exitButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Exit.png")));
-        exitButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/ExitClick.png")));
-        exitButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/ExitHover.png")));
+        exitButtonStyle.up = new TextureRegionDrawable( atlas.findRegion("Exit"));
+        exitButtonStyle.down = new TextureRegionDrawable(atlas.findRegion("ExitClick"));
+        exitButtonStyle.imageOver = new TextureRegionDrawable(atlas.findRegion("ExitHover"));
         final ImageButton exitButton = new ImageButton(exitButtonStyle);
         exitButton.setSize(4 / 14 * Gdx.graphics.getWidth(), 7 / 50 * Gdx.graphics.getHeight());
         exitButton.setPosition(3/14*Gdx.graphics.getWidth(), 0);
 
         ImageButton.ImageButtonStyle loadButtonStyle = new ImageButton.ImageButtonStyle();
-        loadButtonStyle.up = new TextureRegionDrawable( new TextureRegion(new Texture("Button/Load.png")));
-        loadButtonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture("Button/LoadClick.png")));
-        loadButtonStyle.imageOver = new TextureRegionDrawable(new TextureRegion(new Texture("Button/LoadHover.png")));
+        loadButtonStyle.up = new TextureRegionDrawable(atlas.findRegion("Load"));
+        loadButtonStyle.down = new TextureRegionDrawable(atlas.findRegion("LoadClick"));
+        loadButtonStyle.imageOver = new TextureRegionDrawable(atlas.findRegion("LoadHover"));
         final ImageButton loadButton = new ImageButton(loadButtonStyle);
         loadButton.setSize(3 / 14 * Gdx.graphics.getWidth(), 7 / 50 * Gdx.graphics.getHeight());
         loadButton.setPosition(11/14*Gdx.graphics.getWidth(), 0);
