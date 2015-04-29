ba6926c4759fdc1087121ad9a6f0628eec99cd5c
James Dallman <jrdallman@wisc.edu>
2015-04-28 19:13:06 -0500

    Added enemies, edited bullet and gameLevel a little

--- core/src/club/enlight/software/states/GameLevel.java
+++ core/src/club/enlight/software/states/GameLevel.java
@@ -1,28 +1,36 @@
 package club.enlight.software.states;
 
+import club.enlight.software.entities.Enemy.Enemy;
+import club.enlight.software.entities.player.Bullet;
 import club.enlight.software.entities.player.Player;
 import club.enlight.software.handlers.StateManager;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jdallman2570 on 2/23/2015.
  */
 public class GameLevel extends State {
     Player player;
+    ArrayList<Enemy> enemies;
     int width;
     int height;
-
+    List<Bullet> bullets = new ArrayList();
     public GameLevel(final StateManager sm){
         super(sm);
         //Create a level (or load one) using getLevel from sm and Ryan's level generator, along with a room generator,
         //then play the level
-
+        enemies = new ArrayList<Enemy>();
         Vector2 centerOfScreen = new Vector2(0,0); // Orthographic camera centers screen at (0,0)
         width = Gdx.graphics.getWidth();
         height = Gdx.graphics.getHeight();
-        player = new Player(this, centerOfScreen, new OrthographicCamera(width, height));
+        OrthographicCamera camera = new OrthographicCamera(width, height);
+        player = new Player(this, centerOfScreen, camera);
+        enemies.add(new Enemy(this, new Vector2(100f,100f), camera));
     }
     @Override
     public void handleInput() {
@@ -32,11 +40,17 @@ public class GameLevel extends State {
     @Override
     public void update(float dt) {
         player.update(dt);
+        for(Enemy enemy: enemies){
+            enemy.update(dt);
+        }
     }
 
     @Override
     public void render() {
         player.render();
+        for(Enemy enemy: enemies){
+            enemy.render();
+        }
     }
 
     @Override
@@ -50,5 +64,10 @@ public class GameLevel extends State {
 
     public int getHeight() {
         return height;
+    }
+
+    public Vector2 getPlayerPos()
+    {
+        return player.position;
     }
 }
